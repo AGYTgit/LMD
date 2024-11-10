be2b853f25657d0dfd5670eb1a42ca92a46eea78
AGYTgit <bubonb12@gmail.com>
2024-11-10 11:24:24 +0100

    reloader fix

--- waybar/reloader.sh
+++ waybar/reloader.sh
@@ -7,6 +7,6 @@ trap "killall waybar" EXIT
 waybar -c $CONFIG_PATH/$THEME_PATH/config.jsonc -s $CONFIG_PATH/$THEME_PATH/style.css &
 
 while true; do
-    inotifywait -e modify ${CONFIG_PATH}
+    inotifywait -e modify ${CONFIG_PATH}/${THEME_PATH}
     killall -SIGUSR2 waybar
 done
