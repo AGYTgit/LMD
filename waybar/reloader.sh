d9a114cc64c454a117feb9c3f2fc097b37cb5948
AGYTgit <bubonb12@gmail.com>
2024-11-11 15:49:38 +0100

    reloader update

--- waybar/reloader.sh
+++ waybar/reloader.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 
-CONFIG_PATH="${HOME}/.dotfiles/LMD-dotfiles/waybar"
+CONFIG_PATH="$HOME/.dotfiles/LMD-dotfiles/waybar"
 
-trap "killall ${WAYBAR_PID}; exit" SIGTERM
+trap "killall $WAYBAR_PID; exit" SIGTERM
 
 while true; do
-	THEME_PATH="themes/$(cat ${CONFIG_PATH}/current-theme.txt)"
+	THEME_PATH="themes/$(cat $CONFIG_PATH/current-theme.txt)"
 
-    waybar -c "${CONFIG_PATH}/${THEME_PATH}/config.jsonc" -s "${CONFIG_PATH}/${THEME_PATH}/style.css" &
+    waybar -c "$CONFIG_PATH/$THEME_PATH/config.jsonc" -s "$CONFIG_PATH/$THEME_PATH/style.css" &
 	WAYBAR_PID=$!
 
-    inotifywait -e modify,create,move,delete "${CONFIG_PATH}/current-theme.txt" "${CONFIG_PATH}/${THEME_PATH}"
+    inotifywait -e modify,create,move,delete "$CONFIG_PATH/current-theme.txt" "$CONFIG_PATH/$THEME_PATH"
 
-    kill ${WAYBAR_PID}
+    kill $WAYBAR_PID
 done
