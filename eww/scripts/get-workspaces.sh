5221c9d0c572714eb594eb5936f7ce13322104d9
AGYTgit <bubonb12@gmail.com>
2024-11-14 21:37:43 +0100

    better workspaces

--- eww/scripts/get-workspaces.sh
+++ eww/scripts/get-workspaces.sh
@@ -3,22 +3,28 @@
 monitor=$1
 
 spaces () {
-  active_workspace=$(hyprctl activeworkspace -j | jq -c)
+	active_workspace=$(hyprctl activeworkspace -j | jq -c)
 
-  hyprctl workspaces -j | jq -c '[.[] | select(.monitorID == '"$monitor"' and .monitorID == '"$active_workspace.monitorID"') | {
-    id: .id,
-    windows: .windows,
-    active: (.id == '"$active_workspace.id"')
-  }]'
+	workspaces=$(hyprctl workspaces -j | jq -c '[.[] | select(.monitorID == '"$monitor"' and .monitorID == '"$active_workspace.monitorID"') | {
+		id: .id,
+		windows: .windows,
+		active: (.id == '"$active_workspace.id"')
+	}]')
+
+	if [[ "$monitor" == 1 ]]; then
+		echo "$workspaces" | jq -c reverse
+	else
+		echo "$workspaces" | jq -c
+	fi
 }
 
 prev_state=""
 
 socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-  current_state=$(spaces)
+	current_state=$(spaces)
 
-  if [[ "$current_state" != "$prev_state" && "$current_state" != "[]" ]]; then
-    echo "$current_state"
-    prev_state="$current_state"
-  fi
+	if [[ "$current_state" != "$prev_state" && "$current_state" != "[]" ]]; then
+		echo "$current_state"
+		prev_state="$current_state"
+	fi
 done
