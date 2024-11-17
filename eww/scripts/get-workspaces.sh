d11b9dff114f86302c326a2ee270d12bb68faf77
AGYTgit <bubonb12@gmail.com>
2024-11-17 19:10:38 +0100

    added clock widget and better workspace reloading

--- eww/scripts/get-workspaces.sh
+++ eww/scripts/get-workspaces.sh
@@ -4,15 +4,19 @@ monitor=$1
 reverse=$2
 
 prev_state=""
+get_workspaces() {
+    current_state=$(./scripts/get-workspaces "$monitor" "$reverse")
 
-# Run the C program and get the workspaces
-socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-    # Call the compiled get_workspaces program
-    current_state=$(./scripts/get-workspaces "$monitor" "$reverse")  # Pass arguments to get_workspaces
-
-    # Compare and output if the state changes and isn't empty
     if [[ "$current_state" != "$prev_state" && "$current_state" != "[]" ]]; then
         echo "$current_state"
         prev_state="$current_state"
     fi
+}
+
+get_workspaces
+
+socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+	get_workspaces
 done
+
+
