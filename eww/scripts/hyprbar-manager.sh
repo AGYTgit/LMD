738855184787ebde9ac877e8ceb73040fe5db438
AGYTgit <bubonb12@gmail.com>
2024-11-25 21:13:29 +0100

    hyprbar manager for managing current monitor for hyprbar

---
+++ eww/scripts/hyprbar-manager.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+monitor=$1
+
+prev_state=""
+get_fullscreen_monitor() {
+	current_state=$(hyprctl activeworkspace -j | jq -r "select(.monitor == \"$monitor\") | .hasfullscreen")
+
+    if [[ "$current_state" != "$prev_state" && "$current_state" != "[]" ]]; then
+		$(eww close hyprbar)
+		if [[ "$current_state" == false ]]; then
+			$(eww open hyprbar --screen 1)
+		else
+			$(eww open hyprbar --screen 0)
+		fi
+
+		prev_state="$current_state"
+    fi
+}
+
+get_fullscreen_monitor
+
+socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+	get_fullscreen_monitor
+done
