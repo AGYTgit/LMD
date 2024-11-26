3d8ae4736c310a419c223b7fb9fb5e5823c7761a
AGYTgit <bubonb12@gmail.com>
2024-11-26 18:09:00 +0100

    bug fix

--- eww/scripts/hyprbar-manager.sh
+++ eww/scripts/hyprbar-manager.sh
@@ -6,12 +6,12 @@ prev_state=""
 get_fullscreen_monitor() {
 	current_state=$(hyprctl activeworkspace -j | jq -r "select(.monitor == \"$monitor\") | .hasfullscreen")
 
-    if [[ "$current_state" != "$prev_state" && "$current_state" != "[]" ]]; then
-		$(eww close hyprbar)
+    if [[ "$current_state" != "$prev_state" && "$current_state" != '' ]]; then
+		echo $(eww close hyprbar)
 		if [[ "$current_state" == true ]]; then
-			$(eww open hyprbar --screen 0)
+			echo $(eww open hyprbar --screen 0)
 		else
-			$(eww open hyprbar --screen 1)
+			echo $(eww open hyprbar --screen 1)
 		fi
 
 		prev_state="$current_state"
