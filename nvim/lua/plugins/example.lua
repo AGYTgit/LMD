11b7bcad4e0b77c62dc0f765e5df71b7cbbc45e7
AGYTgit <bubonb12@gmail.com>
2025-03-15 11:49:36 +0100

    added parser for c++

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -40,49 +40,50 @@ return {
   },
 
   -- add more treesitter parsers
-  --  {
-  --    "nvim-treesitter/nvim-treesitter",
-  --    opts = {
-  --      ensure_installed = {
-  --        "bash",
-  --        "html",
-  --        "javascript",
-  --        "json",
-  --        "lua",
-  --        "markdown",
-  --        "markdown_inline",
-  --        "python",
-  --        "query",
-  --        "regex",
-  --        "tsx",
-  --        "typescript",
-  --        "vim",
-  --        "yaml",
-  --        "yuck",
-  --      },
-  --    },
-  --  },
-  --
-  --
-  --  {
-  --    "nvim-lualine/lualine.nvim",
-  --    event = "VeryLazy",
-  --    opts = function()
-  --      return {
-  --        --[[add your custom lualine config here]]
-  --      }
-  --    end,
-  --  },
-  --
-  --  {
-  --    "williamboman/mason.nvim",
-  --    opts = {
-  --      ensure_installed = {
-  --        "stylua",
-  --        "shellcheck",
-  --        "shfmt",
-  --        "flake8",
-  --      },
-  --    },
-  --  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "yuck",
+        "cpp",
+        "c++",
+      },
+    },
+  },
+
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   event = "VeryLazy",
+  --   opts = function()
+  --     return {
+  --       --[[add your custom lualine config here]]
+  --     }
+  --   end,
+  -- },
+
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = {
+  --     ensure_installed = {
+  --       "stylua",
+  --       "shellcheck",
+  --       "shfmt",
+  --       "flake8",
+  --     },
+  --   },
+  -- },
 }
