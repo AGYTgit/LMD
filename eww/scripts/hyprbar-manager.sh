85a25b20cdf04286eb390ce2a048d97194de8b0f
AGYTgit <bubonb12@gmail.com>
2025-07-25 21:43:14 +0200

    Revert "merged lap to main"
    This reverts commit 51d544c61ef8930182f86191210cbdd9fb87670d, reversing
    changes made to 9a06d5ed00c99a690fd7a611c1c33585a0e453c1.

---
+++ eww/scripts/hyprbar-manager.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+monitor=$1
+
+prev_state=""
+get_fullscreen_monitor() {
+	current_state=$(hyprctl activeworkspace -j | jq -r "select(.monitor == \"$monitor\") | .hasfullscreen")
+
+    if [[ "$current_state" != "$prev_state" && "$current_state" != '' ]]; then
+		echo $(eww close hyprbar)
+		if [[ "$current_state" == true ]]; then
+			echo $(eww open hyprbar --screen 0)
+		else
+			echo $(eww open hyprbar --screen 1)
+		fi
+
+		prev_state="$current_state"
+    fi
+}
+
+get_fullscreen_monitor
+
+socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+	get_fullscreen_monitor
+done
