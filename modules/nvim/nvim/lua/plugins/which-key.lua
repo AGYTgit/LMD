372c83ba21967dec0a952420bcc01d7f67c0a37d
AGYTgit <bubonb12@gmail.com>
2026-03-19 20:54:39 +0100

    updated nvim config to master branch

---
+++ modules/nvim/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,35 @@
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("which-key").setup({
+			preset = "helix",
+			icons = {
+				mappings = true,
+				keys = {
+					Space = "󱁐",
+					Up = "↑ ",
+					Down = "↓ ",
+					Left = "← ",
+					Right = "→ ",
+					C = "󰘴 ",
+					M = "󰘵 ",
+					S = "󰘶 ",
+					CR = "󰌑 ",
+					Esc = "󱊷 ",
+					Tab = "󰌒 ",
+					BS = "󱃩 ",
+				},
+			},
+			spec = {
+				{ "<leader>t", group = "Telescope" },
+				{ "<leader>h", group = "Harpoon" },
+				{ "<leader>q", group = "Session" },
+				{ "<leader>r", group = "Refactor" },
+				-- { "<leader>x", group = "Diagnostics" },
+			},
+		})
+		vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "WhichKeyBorder", { bg = "none" })
+	end,
+}
