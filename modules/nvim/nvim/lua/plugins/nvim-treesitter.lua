dfa9c4f746769c89db7d73621f7bbf7214c9ac32
AGYTgit <bubonb12@gmail.com>
2026-03-20 12:36:28 +0100

    added keybind for mason and borders to mason menu

--- modules/nvim/nvim/lua/plugins/nvim-treesitter.lua
+++ modules/nvim/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,11 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		lazy = false,
-		main = "nvim-treesitter",
+		config = function(_, opts)
+			local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+			if not status_ok then return end
+			configs.setup(opts)
+		end,
 		opts = {
 			ensure_installed = {
 				"lua",
