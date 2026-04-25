425adbd77385a1ac97cdd969ad6eebfb87d38728
AGYTgit <bubonb12@gmail.com>
2026-04-25 23:05:51 +0200

    .

---
+++ modules/nvim/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+	-- "catppuccin/nvim",
+	-- name = "catppuccin",
+	-- priority = 1,
+	--    require("catppuccin").setup({
+	--        flavour = "mocha", -- latte, frappe, macchiato, mocha
+	--        background = { -- :h background
+	--            light = "mocha",
+	--            dark = "mocha",
+	--        },
+	--        transparent_background = true, -- disables setting the background color.
+	--    }),
+	--    vim.cmd.colorscheme "catppuccin-nvim"
+}
