4c021a436cb800ba151e43c6925c1de1f205e322
AGYTgit <bubonb12@gmail.com>
2026-08-03 13:36:37 +0200

    fixed files not being saved after inc rename

--- modules/nvim/nvim/lua/plugins/inc-rename.lua
+++ modules/nvim/nvim/lua/plugins/inc-rename.lua
@@ -2,7 +2,11 @@ local modName = "[Inc-rename] "
 
 return {
 	"smjonas/inc-rename.nvim",
-	config = function() require("inc_rename").setup({}) end,
+	config = function()
+		require("inc_rename").setup({
+			post_hook = function() vim.cmd("wa") end,
+		})
+	end,
 	keys = {
 		{ "<leader>rn", function() return ":IncRename " .. vim.fn.expand("<cword>") end, mode = "n", expr = true, desc = modName .. "Rename variable" },
 	},
